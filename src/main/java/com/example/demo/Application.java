2762cab984519c239c5d7a1f48a49bf130d3a4f9
sebas031811 <sebas03181@gmail.com>
2024-08-20 16:38:17 -0500

    add vulns

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -3,11 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        String name = "NAME1";
+        String name2 = "NAME2";
     }
 
 }
